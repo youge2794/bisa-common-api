aab9f617233820a4ac2b8429adce4ec1ea179956
qiuyahui <qyh0518@163.com>
2018-10-16 11:28:47 +0800

    更新邮件模板

--- health-common-utils/src/main/java/com/bisa/health/common/email/utils/EmailTemplate.java
+++ health-common-utils/src/main/java/com/bisa/health/common/email/utils/EmailTemplate.java
@@ -9,10 +9,10 @@ import java.util.Date;
  */
 
 public class EmailTemplate {
-	
-	private static final String confirm_order_subject = "悉心康健-订单确认";
-	private static final String cancel_order_subject = "悉心康健-订单取消";
-	private static final String shipping_order_subject = "悉心康健-订单发货";
+	private static final String commit_order_subject="悉心康健-提交訂單";
+	private static final String confirm_order_subject = "悉心康健-訂單確認";
+	private static final String cancel_order_subject = "悉心康健-訂單取消";
+	private static final String shipping_order_subject = "悉心康健-訂單發貨";
 	
 	private static EmailTemplate instance;
 
@@ -39,31 +39,31 @@ public class EmailTemplate {
 		String html = 
 				 "<div><br><br>" 
 				+ "<img style=\"border: currentColor; width: 100px; min-height: auto; float: left;\"> " 
-				+ "<font color=\"#b0a4ae\" style=\"float: right;\">访问碧沙商城</font></div>"
+				+ "<font color=\"#b0a4ae\" style=\"float: right;\">訪問碧沙商城</font></div>"
 				+ "<div align=" + "center" + "style=\"width: 600px; min-height: 500px; background-color: rgb(255, 255, 255);\">"
 				+ "<div align=\"left\" style=\"width: 500px; min-height: 500px; background-color: rgb(255, 255, 255);\"><br> <br> <span style=" + "font-size: 22px;" 
 				+ "> <b>尊敬的" + nickName + "</b></span> <br><br><div style=\" line-height: 1.75; white-space: pre-wrap;\">"
                 + "<div style=\"line-height: 1.75;\">"
                 + state+"</div></div>"
 				+ "<div style=\"line-height: 1.75; white-space: pre-wrap;\"><br></div>"
-				+ "您的订单资讯<br> "
-				+ "碧沙商城订单号码：" + orderNumber + "<br>"
-				+ "订单日期：" + date 
-				+ "<br>订单状态详情链接<br> "
-				+ "<a href="+href+">"+href+"</a>（点开跳商城登陆后显示订单详情）<br>"
-				+ "<br> 如有任何疑问，请发送电子邮件或致电香港客户服务团队<br> "
-				+ "客户服务邮箱："
-				+ "xx@xx.com</a><br>客户服务电话：+123456789<br> "
-				+ "客户服务传真：+123456789<br>"
-				+ "碧莎香港办事处：碧莎科技(香港)有限司香港新界葵涌健康街18號恒亞中心315室<br>"
-				+ "办事处服务时间：格林威治标准时间上午8:00至下午6:30 + 8（周一至周五） </div> </div>"
+				+ "您的訂單資訊<br> "
+				+ "碧沙商城訂單號碼：" + orderNumber + "<br>"
+				+ "訂單日期：" + date
+				+ "<br>訂單狀態詳情鏈接<br> "
+				+ "<a href="+href+">"+href+"</a>（點開跳商城登陸後顯示訂單詳情）<br>"
+				+ "<br> 如有任何疑問，請發送電子郵件或致電香港客戶服務團隊<br> "
+				+ "客戶服務郵箱："
+				+ "xx@xx.com</a><br>客戶服務電話：+123456789<br> "
+				+ "客戶服務傳真：+123456789<br>"
+				+ "碧莎香港辦事處：碧莎科技(香港)有限司香港新界葵湧健康街18號恒亞中心315室<br>"
+				+ "刅倳處棴務溡簡：咯啉媙菭摽痽溡簡仩吘8:00臸芐吘6:30 + 8（淍①臸淍伍） </div> </div>"
 				+ "<div align=\"left\" style=\"width: 600px; margin-top: 2px; min-height: 120px; background-color: rgb(255, 255, 255);\" >" 
 				+ "<div align=\"left\" style=\"width: 500px; min-height: 120px; background-color: rgb(255, 255, 255);\"><br> "
 				+ "<span style=\"color: rgb(176, 176, 176); font-size: 12px;\">"
-				+ "本邮件由碧莎系统自动发出，请勿直接回复！"
-				+ "<br>因碧莎商城最终页面的产品或服务的信息（包括但不限于产品型号、价格、促销等）可能会发生变动，请以您点击本邮件中的商品链接到碧莎商城最终页面上显示的该商品或服务的信息为准。"
+				+ "本郵件由碧莎系統自動發出，請勿直接回復！"
+				+ "<br>因碧莎商城最終頁面的產品或服務的信息（包括但不限於產品型號、價格、促銷等）可能會發生變動，請以您點擊本郵件中的商品鏈接到碧莎商城最終頁面上顯示的該商品或服務的信息為準。"
 				+ "</span></div></div> <br> <span style=\"color: rgb(157, 164, 174); font-size: 10px;"
-				+ ">Copyright©2015 碧莎科技有限公司 版权所有</span></div>";
+				+ ">Copyright©2015 碧莎科技有限公司 版權所有</span></div>";
 		return html;
 	}
 
@@ -73,12 +73,14 @@ public class EmailTemplate {
 	 * @return
 	 */
 	public String getEmailSubject(int emailType) {
-		if(emailType == 1) {
+		 if(emailType == 1) {
 			return confirm_order_subject;
 		}else if(emailType == 2) {
 			return shipping_order_subject;
 		}else if(emailType == 3) {
 			return cancel_order_subject;
+		}else if (emailType ==4){
+			return commit_order_subject;
 		}
 		return "";
 	}
@@ -89,12 +91,15 @@ public class EmailTemplate {
 	 * @return
 	 */
 	public String getBillMessage(int emailType) {
+
 		if(emailType == 1) {
-			return "感谢您订购碧莎产品，您的订单已支付，您可以登入碧莎商城账户或点击下面订单资讯链接查看订单资讯。";
+			return "感謝您訂購碧莎產品，您的訂單已支付，您可以登入碧莎商城賬戶或點擊下面訂單資訊鏈接查看訂單資訊。";
 		}else if(emailType == 2) {
-			return "感谢您订购碧莎产品，您的订单已发货，请关注物流信息，您可以登入碧莎商城账户或点击下面订单资讯链接查看订单资讯。";
+			return "感謝您訂購碧莎產品，您的訂單已發貨，請關註物流信息，您可以登入碧莎商城賬戶或點擊下面訂單資訊鏈接查看訂單資訊。";
 		}else if(emailType == 3) {
-			return "感谢您订购碧莎产品，您的订单已取消，取消原因：（主动取消订单/缺货或者无法配送/物流到付账号有误），您可以登入碧莎商城账户或点击下面订单资讯链接查看订单资讯。";
+			return "感謝您訂購碧莎產品，您的訂單已取消，取消原因：（主動取消訂單/缺貨或者無法配送/物流到付賬號有誤），您可以登入碧莎商城賬戶或點擊下面訂單資訊鏈接查看訂單資訊。";
+		}else if (emailType==4){
+			return "感謝您訂購碧莎產品，您的訂單已提交，請在12小時內支付，您可以登入碧莎商城賬戶或點擊下面訂單資訊鏈接查看訂單資訊。";
 		}
 		return "";
 	}
