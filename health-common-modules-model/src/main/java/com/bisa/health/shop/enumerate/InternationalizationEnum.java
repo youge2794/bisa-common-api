f46980393656ad5383194d545eff967fe6a645a8
15270673102 <15270673102@163.com>
2018-11-22 10:05:03 +0800

    更新 国际化枚举

--- health-common-modules-model/src/main/java/com/bisa/health/shop/enumerate/InternationalizationEnum.java
+++ health-common-modules-model/src/main/java/com/bisa/health/shop/enumerate/InternationalizationEnum.java
@@ -10,18 +10,27 @@ public enum InternationalizationEnum {
     /**
      * 中文简体
      */
-    zh_CN(1, "zh_CN"),
+    zh_CN(1, "zh_CN", "简体"),
     /**
      * 中文繁体
      */
-    zh_HK(2, "zh_HK"),
+    zh_HK(2, "zh_HK", "繁体"),
     /**
      * 英文
      */
-    en_US(3, "en_US");
+    en_US(3, "en_US", "英文");
 
     private int value;
     private String name;
+    private String msg;
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
 
     public int getValue() {
         return value;
@@ -39,9 +48,10 @@ public enum InternationalizationEnum {
         this.name = name;
     }
 
-    private InternationalizationEnum(int value, String name) {
+    private InternationalizationEnum(int value, String name, String msg) {
         this.value = value;
         this.name = name;
+        this.msg = msg;
     }
 
     public static InternationalizationEnum getByValue(int value) {
